02f4657990092c9b6cf46201f448adba08c604c5
NamanArora <naman.arora11@gmail.com>
2017-10-03 20:17:02 +0530

    Misc

--- App/Containers/Styles/LoginScreenStyle.js
+++ App/Containers/Styles/LoginScreenStyle.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from 'react-native'
 import { ApplicationStyles } from '../../Themes/'
-
+import Colors from '../../Themes/Colors'
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  heading:{
+    fontSize: 25,
+    color: Colors.text
+
+  }
 })
