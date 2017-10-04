15f156d0bfb02ec9c9407d17d7f2661df1e719de
NamanArora <naman.arora11@gmail.com>
2017-10-04 19:51:13 +0530

    Added tabs

--- App/Containers/ExistingChatsScreen.js
+++ App/Containers/ExistingChatsScreen.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
+import { StackNavigator, TabNavigator } from 'react-navigation'
+import LoginScreen from '../Containers/LoginScreen'
+import LaunchScreen from '../Containers/LaunchScreen'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
 // Styles
 import styles from './Styles/ExistingChatsScreenStyle'
 
-class ExistingChatsScreen extends Component {
-  render () {
-    return (
-      <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>ExistingChatsScreen</Text>
-        </KeyboardAvoidingView>
-      </ScrollView>
-    )
-  }
-}
 
-const mapStateToProps = (state) => {
-  return {
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ExistingChatsScreen)
+export const Exist= TabNavigator({
+  Chats: { screen: LoginScreen },
+  Contacts: { screen: LaunchScreen }
+});
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,24 +1,28 @@
-import { StackNavigator } from 'react-navigation'
-import ExistingChatsScreen from '../Containers/ExistingChatsScreen'
+import { StackNavigator, TabNavigator } from 'react-navigation'
+
 import QRcodeScreen from '../Containers/QRcodeScreen'
 import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
-
+import {Exist} from '../Containers/ExistingChatsScreen'
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
+
+
 const PrimaryNav = StackNavigator({
-  ExistingChatsScreen: { screen: ExistingChatsScreen },
   QRcodeScreen: { screen: QRcodeScreen },
   LoginScreen: { screen: LoginScreen },
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  ExistingChatsScreen: { screen: Exist },
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'QRcodeScreen',
+  initialRouteName: 'LoginScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
 })
+
+
 
 export default PrimaryNav
