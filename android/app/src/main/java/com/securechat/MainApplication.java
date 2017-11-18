643209092b76783142c357a7ad712d5c28ef7a12
NamanArora <naman.arora11@gmail.com>
2017-11-18 16:42:47 +0530

    Fix firebase db in MainApplication

--- android/app/src/main/java/com/securechat/MainApplication.java
+++ android/app/src/main/java/com/securechat/MainApplication.java
@@ -3,6 +3,8 @@ package com.securechat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,11 +30,13 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RCTCameraPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
             new RNDeviceInfo(),
-            new SvgPackage()
+            new SvgPackage(),
+            new RNFirebaseDatabasePackage()
       );
     }
   };
