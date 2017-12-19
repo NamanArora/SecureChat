0702a227cdda55d6f162859b8f323b97a5676bbd
NamanArora <naman.arora11@gmail.com>
2017-12-20 04:08:48 +0530

    Final Commit

--- App/Containers/Chats.js
+++ App/Containers/Chats.js
@@ -57,23 +57,9 @@ class Chats extends Component {
     
   }
 
-
-  /* ***********************************************************
-  * STEP 1
-  * This is an array of objects with the properties you desire
-  * Usually this should come from Redux mapStateToProps
-  *************************************************************/
     dataObjects= [
     ]
 
-  /* ***********************************************************
-  * STEP 2
-  * `renderRow` function. How each cell/row should be rendered
-  * It's our best practice to place a single component here:
-  *
-  * e.g.
-    return <MyCustomCell title={item.title} description={item.description} />
-  *************************************************************/
     
   renderRow ({item}) {
     return (
@@ -91,12 +77,6 @@ class Chats extends Component {
     navigation.navigate('ChatScreen', {user: this.state.username, friend: f});
   }
 
-  /* ***********************************************************
-  * STEP 3
-  * Consider the configurations we've set below.  Customize them
-  * to your liking!  Each with some friendly advice.
-  *************************************************************/
-  // Render a header?
   renderHeader = () =>
     <Text style={[styles.label, styles.sectionHeader]}> - Header - </Text>
 
@@ -116,27 +96,12 @@ class Chats extends Component {
   }}
 />
 
-  // The default function if no Key is provided is index
-  // an identifiable key is important if you plan on
-  // item reordering.  Otherwise index is fine
   keyExtractor = (item, index) => index
 
   // How many items should be kept im memory as we scroll?
   oneScreensWorth = 20
 
-  // extraData is for anything that is not indicated in data
-  // for instance, if you kept "favorites" in `this.state.favs`
-  // pass that in, so changes in favorites will cause a re-render
-  // and your renderItem will have access to change depending on state
-  // e.g. `extraData`={this.state.favs}
 
-  // Optimize your list if the height of each item can be calculated
-  // by supplying a constant height, there is no need to measure each
-  // item after it renders.  This can save significant time for lists
-  // of a size 100+
-  // e.g. itemLayout={(data, index) => (
-  //   {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
-  // )}
 
   render () {
     return (
